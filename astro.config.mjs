36bfc78a8dacb473faa53f08f84cbb529510e293
rin-yato <chearithorn@gmail.com>
2023-08-18 20:51:08 +0700

    chore: included file for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://querymaster.io',
   integrations: [tailwind(), sitemap()],
-  adapter: vercel(),
+  adapter: vercel({
+    includeFiles: [
+      './public/icon.svg',
+      './public/QueryMaster.png',
+      './public/QueryMaster-half.png',
+    ],
+  }),
   output: 'hybrid',
 });
