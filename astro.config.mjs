920659f1fcf80325cea347670c630e140dacacc9
rin-yato <chearithorn@gmail.com>
2023-08-18 20:06:34 +0700

    hotfix: updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
-import compress from 'astro-compress';
-import partytown from '@astrojs/partytown';
-import sitemap from '@astrojs/sitemap';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), compress(), partytown(), sitemap()],
+  integrations: [tailwind(), image()],
   adapter: vercel(),
-  output: 'hybrid'
+  output: 'hybrid',
 });
