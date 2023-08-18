090783191d9c8ebd7fa55dda61473d36f6270d31
rin-yato <chearithorn@gmail.com>
2023-08-18 20:35:23 +0700

    chore: removed unneeded .env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
 
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), sitemap()],
   adapter: vercel(),
-  output: 'server',
+  output: 'hybrid',
 });
